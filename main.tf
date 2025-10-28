1085ed5ad45ae1436a382ce68102fab6e5f95994
kanchichaitanya1994-source <kanchichaitanya1994@gmail.com>
2025-10-28 11:42:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-02d26659fd82cf299"
   instance_type          = "t3.micro"
-  key_name               = "Netflix"
+  key_name               = "jn"
   vpc_security_group_ids = ["sg-00c55c8fe13c6465d"]
   tags = {
     Name = var.instance_names[count.index]
